970d8b18ef0400a24141e0fd1abe16b8cb3cae93
VEDANT SHROTRIA <40681425+Jonsy13@users.noreply.github.com>
2021-06-10 12:57:38 +0530

    Update filter_and_build.sh

--- .github/filter_and_build.sh
+++ .github/filter_and_build.sh
@@ -5,5 +5,5 @@ working_dir="litmus-portal"
 
 declare -A MYMAP=( [frontend]="litmusportal-frontend:ci" [graphql-server]="litmusportal-server:ci" [authentication]="litmusportal-auth-server:ci" )
 # Building the images on the basic of changes in paths
-current_dir=$(echo "$working_dir/$directory}")
+current_dir=$(echo "$working_dir/$directory")
 docker build $current_dir -t litmuschaos/${MYMAP[$directory]} -f $working_dir/${directory}/Dockerfile
