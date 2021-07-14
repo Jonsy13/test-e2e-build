50e216534ee31bffcb9576c39119e9e6d64dc435
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-07-14 18:50:29 +0530

    Added frontend build.
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- .github/filter_and_build.sh
+++ .github/filter_and_build.sh
@@ -4,9 +4,8 @@ set -e
 working_dir="litmus-portal"
 
 declare -A MYMAP=( [frontend]="litmusportal-frontend:ci" [graphql-server]="litmusportal-server:ci" [authentication]="litmusportal-auth-server:ci" )
-
-# Building the images on the basic of changes in paths and saving the tar versions into Images directory
+# Building the images on the basic of changes in paths
 current_dir=$(echo "$working_dir/$directory")
 mkdir Images
-DOCKER_BUILDKIT=1 docker build $current_dir -t litmuschaos/${MYMAP[$directory]} -f $working_dir/${directory}/Dockerfile
+docker build $current_dir -t litmuschaos/${MYMAP[$directory]} -f $working_dir/${directory}/Dockerfile
 docker save "litmuschaos/${MYMAP[$directory]}" > Images/${directory}.tar
